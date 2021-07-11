767b3f90e9208720b799a118d068a0f9b7b8312f
Edward Hachem <edwardahachem@gmail.com>
2021-07-11 10:38:09 -0400

    Fixed errors and started the cases for parsing.

--- BasicParser.cpp
+++ BasicParser.cpp
@@ -2,6 +2,7 @@
 #include "Identifier.h"
 #include <list>
 #include <map>
+#include <cassert>
 using namespace std;
 
 void BasicParser::init(list<BasicToken> *tokens, map<string, Identifier> *identifiers)
@@ -20,22 +21,28 @@ void BasicParser::parse()
 
 /// <summary>
 ///		parses the following BNF
-/// 	<Line>        :: = Integer <Statements> 
+/// <Line> --> Number_Literal <Statements> NewLine <Line>
+///				| Number_Literal <Statements> NewLine
 /// </summary>
 void BasicParser::parseLine()
 {
-	BasicToken token = *_tokenIt;
-	// must be programline number
-	if (token.type == TokenType::PROGRAMLINENUMBER) {
-		int lineNumber = stoi(token.lexeme);
-		// todo:
-		// do something with the line number
-		// we'll need to put it as the P table so we can go to it for some conditions
-		_tokenIt++; // advance to next token
-		parseStatements();
-	}
-	else {
-		error("error: expected basic line Number. line:", token.linenumber);
+	while (_tokenIt != _ptokens->end())
+	{
+		BasicToken token = *_tokenIt;
+		// must be programline number
+		if (token.type == TokenType::PROGRAMLINENUMBER) {
+			int lineNumber = stoi(token.lexeme);
+			// we'll need to put it as the P table so we can go to it for some conditions
+			// save it in a lookup to the current Px index, the address of this line in the P array 
+			ProgramLineNumberToPx[lineNumber] = Px;
+
+			_tokenIt++; // advance to next token
+			parseStatements();
+		}
+		else {
+			_tokenIt++;
+			error("error: expected basic line Number. line:", token.linenumber);
+		}
 	}
 }
 
@@ -51,11 +58,15 @@ void BasicParser::parseStatements()
 	// now token iterator should be after statement.
 	// if there's divider, continue parsing next statement
 	BasicToken token = *_tokenIt;
-	while (token.lexeme.find_first_of(dividers) >= 0)
+	while ( (_tokenIt != _ptokens->end() && 
+			(*_tokenIt).type != TokenType::EOL) &&
+		token.lexeme.find_first_of(dividers) >= 0)
 	{
 		_tokenIt++;
 		parseStatement();
 	}
+	if ((*_tokenIt).type == TokenType::EOL)
+		_tokenIt++;
 }
 
 /// <summary>
@@ -92,6 +103,7 @@ void BasicParser::parseStatement()
 		break;
 
 	case TokenType::FOR:
+
 		break;
 
 	case TokenType::GOTO:
@@ -101,30 +113,66 @@ void BasicParser::parseStatement()
 		break;
 
 	case TokenType::IF:
-		//IF <Expression> THEN Number_Literal
+	{	//IF <Expression> THEN Number_Literal
+		// In P table, this is pushed in this order:
+		//	Expr, 
+		//	Number_Literal (the lineNumber to go to if true)
+		//	BP (branches to the true address if result is true), 
 		_tokenIt++;
 		int exp1 = parseExpression();
+		// pushed by the expression
 		if ((*_tokenIt).type == TokenType::THEN)
 		{
 			_tokenIt++;
-			int gotoLinenumber = stoi((*_tokenIt).lexeme);
+			int codeLineNumber = stoi((*_tokenIt).lexeme);
+			pushP(CMD::LINENUMBER, codeLineNumber);
+			pushP(CMD::BP, NULL);
 		}
 		else
 		{
 			error("Expected \"then\" ", ((*_tokenIt).linenumber));
 		}
 		break;
-
+	}
 	case TokenType::INPUT:
 		break;
 
 	case TokenType::LET:
-		break;
+	{
+		///	 LET Identifier = <Expression>
+		// pushed like this:
+		// identifier, expression, assignOP
+		// 
+		string name; //identifer name
+		int value;
+		_tokenIt++;
+		if ((*_tokenIt).type == TokenType::IDENTIFIER)
+		{
+			name = (*_tokenIt).lexeme;
+			Identifier* pid = &(*_pidentifiers)[name];
+			pushP(CMD::IDENTIFIER, pid);
 
+			_tokenIt++;
+			if ((*_tokenIt).type == TokenType::EQUAL)
+			{
+				_tokenIt++;
+				value = parseExpression();
+				// pushed by expression
+				pushP(CMD::ASSIGN);
+			}
+		}
+		else
+		{
+			error("Expected \"then\" ", ((*_tokenIt).linenumber));
+		}
+		break;
+	}
 	case TokenType::NEXT:
 		break;
 
 	case TokenType::PRINT:
+		// push identifier pointer
+		// push print cmd
 		break;
 
 	case TokenType::READ:
@@ -134,10 +182,14 @@ void BasicParser::parseStatement()
 		break;
 
 	case TokenType::REM:
+		// consume and ignore all tokens on the line
+		while (_tokenIt != _ptokens->end() && (*_tokenIt).type!=TokenType::EOL)
+			_tokenIt++;
 		break;
 
 	default:
 		//Check function expression, otherwise error.
+		break;
 	}
 }
 /// <summary>
@@ -148,13 +200,15 @@ void BasicParser::parseStatement()
 int BasicParser::parseExpression()
 {
 	BasicToken token = *_tokenIt;
-	parseAndExpression();
+	int left = parseAndExpression();
 	if ((*_tokenIt).type == TokenType::OR)
 	{
 		_tokenIt++;
-		parseExpression();
+		int right = parseExpression();
+		return left || right;
 	}
-	return 0;
+	
+	return left;
 }
 
 /// <summary>
@@ -165,13 +219,14 @@ int BasicParser::parseExpression()
 int BasicParser::parseAndExpression()
 {
 	BasicToken token = *_tokenIt;
-	parseNotExpression();
+	int left = parseNotExpression();
 	if ((*_tokenIt).type == TokenType::AND)
 	{
 		_tokenIt++;
-		parseAndExpression();
+		int right = parseAndExpression();
+		return left && right;
 	}
-	return 0;
+	return left;
 }
 
 /// <summary>
@@ -185,11 +240,11 @@ int BasicParser::parseNotExpression()
 	if ((*_tokenIt).type == TokenType::NOT)
 	{
 		_tokenIt++;
-		parseCompareExpression();
+		return !parseCompareExpression();
 	}
 	else
 	{
-		parseCompareExpression();
+		return parseCompareExpression();
 	}
 	return 0;
 }
@@ -207,22 +262,44 @@ int BasicParser::parseNotExpression()
 int BasicParser::parseCompareExpression()
 {
 	BasicToken token = *_tokenIt;
-	parseAddExpression();
+	int right;
+	int left = parseAddExpression();
 	switch ((*_tokenIt).type)
 	{
 	case TokenType::EQUAL:
+		_tokenIt++;
+		right = parseCompareExpression();
+		return left == right;
+		break;
 	case TokenType::GREATERTHAN:
+		_tokenIt++;
+		right = parseCompareExpression();
+		return left > right;
+		break;
 	case TokenType::GREATERTHANOREQUALTO:
+		_tokenIt++;
+		right = parseCompareExpression();
+		return left >= right;
+		break;
 	case TokenType::NOTEQUAL:
+		_tokenIt++;
+		right = parseCompareExpression();
+		return left != right;
+		break;
 	case TokenType::LESSTHAN:
+		_tokenIt++;
+		right = parseCompareExpression();
+		return left < right;
+		break;
 	case TokenType::LESSTHANOREQUALTO:
 		_tokenIt++;
-		parseCompareExpression();
+		right = parseCompareExpression();
+		return left <= right;
 		break;
 	default:
 		break;
 	}
-	return 0;
+	return left;
 }
 
 /// <summary>
@@ -234,19 +311,26 @@ int BasicParser::parseCompareExpression()
 int BasicParser::parseAddExpression()
 {
 	BasicToken token = *_tokenIt;
-	parseMultExpression();
+	int left = parseMultExpression();
+	int right;
 	switch ((*_tokenIt).type)
 	{
 	case TokenType::ADD:
+		//put the (*_tokenIt).type in parse tree
+		_tokenIt++;
+		right = parseAddExpression();
+		return left + right;
+		break;
 	case TokenType::SUBTRACT:
 		//put the (*_tokenIt).type in parse tree
 		_tokenIt++;
-		parseAddExpression();
+		right = parseAddExpression();
+		return left - right;
 		break;
 	default:
 		break;
 	}
-	return 0;
+	return left;
 }
 
 /// <summary>
@@ -258,16 +342,28 @@ int BasicParser::parseAddExpression()
 int BasicParser::parseMultExpression()
 {
 	BasicToken token = *_tokenIt;
-	parseNegateExpression();
+	int left = parseNegateExpression();
+	// already pushed
+	int right;
 	switch ((*_tokenIt).type)
 	{
 	case TokenType::MULTIPLY:
+		//put the (*_tokenIt).type in parse tree
+		_tokenIt++;
+		right = parseMultExpression();
+		// already pushed. push operator
+		pushP(CMD::MULT);
+		break;
+
 	case TokenType::DIVIDE:
 		//put the (*_tokenIt).type in parse tree
 		_tokenIt++;
-		parseMultExpression();
+		right = parseMultExpression();
+		// already pushed. push operator
+		pushP(CMD::DIV);
 		break;
 	default:
+		return left;
 		break;
 	}
 	return 0;
@@ -285,11 +381,11 @@ int BasicParser::parseNegateExpression()
 	{
 		//put negative lexeme into parse tree
 		_tokenIt++;
-		parseValue();
+		return parseValue();
 	}
 	else
 	{
-		parseValue();
+		return parseValue();
 	}
 
 	return 0;
@@ -312,30 +408,43 @@ int BasicParser::parseValue()
 	case TokenType::LEFTPAREN:
 		//put the (*_tokenIt).type in parse tree
 		_tokenIt++;
-		parseExpression();
-		break;
-	case TokenType::RIGHTPAREN:
-		//put the (*_tokenIt).type in parse tree
+		return parseExpression();
 		//assert that there was a leftparen token type in parse tree before
-		//otherwise throw an error
-		//unsure what to do after a right paren, do we parse the next token at the expression level?	
+		assert((*_tokenIt).type == TokenType::RIGHTPAREN);
+		_tokenIt++;
 		break;
 	case TokenType::IDENTIFIER:
-		_tokenIt++;
-		//save name of identifier here
-		string identifier = token.lexeme;
-		parseValue();
-		break;
-	case TokenType::NUMBERLITERAL:
-
-		break;
+	case TokenType::NUMBERINTLITERAL:
+	case TokenType::NUMBERDBLLITERAL:
 	case TokenType::STRINGLITERAL:
+	{
+		// these are all stored in teh identifier table, including constants and strings
+		// the interpreter will have the type info to figure it out
+		_tokenIt++;
+		Identifier *pIdentifier = &(*_pidentifiers)[token.lexeme];
+		pushP(CMD::IDENTIFIER, pIdentifier);
 		break;
+	}
 	default:
 		break;
 	}
-
 	
 	return 0;
+}
+
+void BasicParser::pushP(CMD cmd, Identifier* ptr)
+{
+	P_struct pEntry;
+	pEntry.cmd = cmd;
+	pEntry.ptr = ptr;
+	P[Px++] = pEntry;
+}
+
+void BasicParser::pushP(CMD cmd , int value)
+{
+	P_struct pEntry;
+	pEntry.cmd = cmd;
+	pEntry.value = value;
+	P[Px++] = pEntry;
 }
 
--- Identifier.cpp
+++ Identifier.cpp
@@ -2,8 +2,10 @@
 
 ostream& operator<<(ostream& os, const Identifier& identifier)
 {
-    if (identifier.identifierType == TokenType::NUMBERLITERAL)
-        os << "Name: " << identifier.name << " Type: " << (int) identifier.identifierType << " Value: " << identifier.valueDouble << endl;
+    if (identifier.identifierType == TokenType::NUMBERDBLLITERAL)
+        os << "Name: " << identifier.name << " Type: " << (int)identifier.identifierType << " Value: " << identifier.valueDouble << endl;
+    else if (identifier.identifierType == TokenType::NUMBERINTLITERAL)
+        os << "Name: " << identifier.name << " Type: " << (int)identifier.identifierType << " Value: " << identifier.valueInt << endl;
     else
         os << "Name: " << identifier.name << " Type: " << (int)identifier.identifierType << " Value: " << identifier.valueString << endl;
 
