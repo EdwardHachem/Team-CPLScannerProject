7c141a8b67a09cfca87e4046280edac7d639cd7d
arizonagranger <42453889+arizonagranger@users.noreply.github.com>
2021-07-12 09:16:33 -0400

    file input added

--- CPLScannerProject.cpp
+++ CPLScannerProject.cpp
@@ -7,6 +7,10 @@ using namespace std;
 int main()
 {	
 	LanguageProcessor languageProcessor;
+	cout<< "Enter a file name: ,default is BASIC_Input_File_1.bas"<<endl;
+	string defaultname = "BASIC_Input_File_1.bas";
+	string fname;
+	cin>>fname;
 	languageProcessor.processFile("BASIC_Input_File_1.bas");
 	return 0;
 }
